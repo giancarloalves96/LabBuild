1ceddc877e2666318ac946062c745903fa7e2338
Giancarlo Alves <giancarlo.alves96@gmail.com>
2017-04-17 14:06:44 -0300

    First commit

---
+++ src/main/java/br/ita/prog/App.java
@@ -0,0 +1,17 @@
+package br.ita.prog;
+
+import com.google.gson.Gson;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Gson gson = new Gson();
+        System.out.println(gson.toJson("Hello World!"));
+    }
+}
